364191037112f4be5c9ea3630a2c1aeead274e02
utkarshLB <utkarsh.gupta@liquiditybook.com>
2025-06-27 03:06:13 +0530

    Initial commit - Fixify app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // enable app directory
+    },
+  };
+  
+  module.exports = nextConfig;
+  
