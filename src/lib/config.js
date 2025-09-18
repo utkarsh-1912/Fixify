8027e997b3c7d3e8c07212fd23b5cbe9f92d988e
Utkarsh Gupta <79002186+utkarsh-1912@users.noreply.github.com>
2025-09-18 15:09:39 +0530

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -5,4 +5,5 @@ export const MODEL_MAP = {
   falcon: "tiiuae/falcon-7b-instruct",
   gptj: "EleutherAI/gpt-j-6B",
   bloom: "bigscience/bloom-7b1",
+  apertus:"swiss-ai/Apertus-8B-Instruct-2509"
 };
